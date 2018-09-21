9fa2db42a9d158b3ee92e98c6ca842aefe143951
JJ Kavelaars <jjk@uvic.ca>
2018-09-20 21:34:28 -0700

    Add date constraint for alma query

---
+++ aq.py
@@ -0,0 +1,44 @@
+from astroquery.alma import AlmaClass
+
+
+class CADCAlmaClass(AlmaClass):
+
+    def query_obsdate_async(self, oldest_observation, newest_observation, cache=True, public=True,
+                            science=True, payload=None, **kwargs):
+        """
+        Query the archive within a given observation date range.
+
+        Parameters
+        ----------
+        oldest_observation : Time
+            The oldest data set to include in result set.
+        newest_observation : Time
+            The most recent data set to include in result set.
+        cache : bool
+            Cache the query?
+        public : bool
+            Return only publicly available datasets?
+        science : bool
+            Return only data marked as "science" in the archive?
+        payload : dict
+            Dictionary of additional keywords.  See `help`.
+        kwargs : dict
+            Passed to `query_async`
+        """
+
+        if payload is None:
+            payload = {}
+        d1 = oldest_observation.to_datetime().timetuple()
+        d2 = newest_observation.to_datetime().timetuple()
+        payload.update({'start_date': "{}-{}-{}..{}-{}-{}".format(d1.tm_mday, d1.tm_mon, d1.tm_year,
+                                                                  d2.tm_mday, d2.tm_mon, d2.tm_year)})
+        print payload
+        return self.query_async(payload, cache=cache, public=public,
+                                science=science, **kwargs)
+
+
+if __name__ == "__main__":
+    from astropy.time import Time
+    oldest_data = Time("2016-01-01 00:00:00", out_subfmt='date')
+    newest_data = Time("2016-02-01 00:00:00", out_subfmt='date')
+    print CADCAlmaClass().query_obsdate_async(oldest_data, newest_data)
