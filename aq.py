85174cd7d04f3a9264d6e72c670400b1f1df05ad
JJ Kavelaars <jjkavelaars@gmail.com>
2018-11-13 14:35:52 -0800

    Make the date range command have a space between start and end date in string

--- aq.py
+++ aq.py
@@ -4,7 +4,7 @@ from astroquery.alma import AlmaClass
 class CADCAlmaClass(AlmaClass):
 
     def query_obsdate_async(self, oldest_observation, newest_observation, cache=True, public=True,
-                            science=True, payload=None, **kwargs):
+                            science=False, payload=None, **kwargs):
         """
         Query the archive within a given observation date range.
 
@@ -30,15 +30,15 @@ class CADCAlmaClass(AlmaClass):
             payload = {}
         d1 = oldest_observation.to_datetime().timetuple()
         d2 = newest_observation.to_datetime().timetuple()
-        payload.update({'start_date': "{}-{}-{}..{}-{}-{}".format(d1.tm_mday, d1.tm_mon, d1.tm_year,
+        payload.update({'start_date': "{:02d}-{:02d}-{:4d} .. {:02d}-{:02d}-{:4d}".format(d1.tm_mday, d1.tm_mon, d1.tm_year,
                                                                   d2.tm_mday, d2.tm_mon, d2.tm_year)})
-        print payload
+        #print payload
         return self.query_async(payload, cache=cache, public=public,
                                 science=science, **kwargs)
 
 
 if __name__ == "__main__":
     from astropy.time import Time
-    oldest_data = Time("2016-01-01 00:00:00", out_subfmt='date')
-    newest_data = Time("2016-02-01 00:00:00", out_subfmt='date')
-    print CADCAlmaClass().query_obsdate_async(oldest_data, newest_data)
+    oldest_data = Time("2015-01-01 10:00:00", out_subfmt='date')
+    newest_data = Time("2016-12-31 10:00:00", out_subfmt='date')
+    print CADCAlmaClass().query_obsdate_async(oldest_data, newest_data).content
