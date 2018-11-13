7f1ca83f4070f5dd995b90769114857f6f2a3416
JJ Kavelaars <jjkavelaars@gmail.com>
2018-11-13 21:32:54 +0000

    Make request list unique and provide some output while running

--- get_alma.py
+++ get_alma.py
@@ -1,13 +1,20 @@
 #!python
+"""Given an ALMA project code on the commandline download all the associated Member observations sets"""
 
 from astroquery.alma import Alma
-import sys
+import sys, os
+import numpy as np
 
 a = Alma()
-a.cache_location="/home/cache/"
+a.cache_location=os.curdir
 
 q = a.query(payload={"project_code": sys.argv[1]})
+uids =  np.unique(q['Member ous id'])
+print("Found {} sets to download".format(len(uids)))
+print(uids)
 
-for uid in q['Member ous id']:
+
+for uid in uids:
+   print("Getting UID: {}".format(uid))
    a.retrieve_data_from_uid([uid,], cache=False)
 
